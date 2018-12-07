b1d92ab41252d4b6731305327a7b9e5e3ba53478
Sir-Apex <45063448+Sir-Apex@users.noreply.github.com>
2018-12-07 23:58:45 +0700

    Update task3.sh

--- Informatics-1 sem/lab3/exp/task3.sh
+++ Informatics-1 sem/lab3/exp/task3.sh
@@ -2,6 +2,6 @@
 
 wget http://factorized.net/patterns.txt
 
-egrep -w '\b[ABEKMHOPCTYX][0-9]{3}[ABEKMHOPCTYX]{2}[0-9]{2,3}\b' patterns.txt
+egrep '\b[ABEKMHOPCTYX][0-9]{3}[ABEKMHOPCTYX]{2}[0-9]{2,3}\b' patterns.txt
 
 rm patterns.txt
