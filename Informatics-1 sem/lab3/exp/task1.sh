9f01e7b4bb2c0d9bd9573d3abe1a494167ee5fc7
Sir-Apex <45063448+Sir-Apex@users.noreply.github.com>
2018-12-07 23:37:40 +0700

    Update task1.sh

--- Informatics-1 sem/lab3/exp/task1.sh
+++ Informatics-1 sem/lab3/exp/task1.sh
@@ -4,7 +4,7 @@ wget  http://factorized.net/crusoe.txt
 
 egrep -o '\bz\w*\b' crusoe.txt 
 
-egrep -o '\b\w{16,16}\b' crusoe.txt
+egrep -o '\b\w{16}\b' crusoe.txt
 
 egrep -o '\ba\w*[c]\b' crusoe.txt
 
